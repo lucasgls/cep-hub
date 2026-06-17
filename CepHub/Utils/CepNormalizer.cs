ad82761f158883b0cafed0635ba35d0f4cff1f86
lucasgls <lucasglsilva7@gmail.com>
2026-06-17 17:24:45 -0300

    feat: implementa classes auxiliares

---
+++ CepHub/Utils/CepNormalizer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CepHub.Utils
+{
+    public class CepNormalizer
+    {
+        public string Normalizar(string cep)
+        { 
+            if (string.IsNullOrEmpty(cep))
+                throw new ArgumentException("O CEP não pode ser nulo, conter espacos em branco ou vazio.");
+            
+            string cepFormatado = Regex.Replace(cep, @"\D", ""); // Remove caracteres não numéricos
+
+            if(cepFormatado.Length != 8)
+                throw new ArgumentException("O CEP deve conter exatamente 8 dígitos numéricos.");
+
+            return cepFormatado;
+        } 
+    }
+}
---
+++ CepHub/Utils/Logger.cs
@@ -0,0 +1,42 @@
+﻿using CepHub.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CepHub.Utils
+{
+    public class Logger
+    {
+        private readonly string _caminho;
+        public Logger()
+        {
+            _caminho = @"C:\Users\LucasGabrielLimaSilv\CSharp\CepHub\CepHub\Data\RegistroLogs.txt";
+        }
+
+        public void GravarLog(ViaCepDto endereco)
+        {
+            string log = $"[{DateTime.Now:dd-MM-yyyy HH:mm:ss}] " + $"{endereco.cep} | {endereco.logradouro}, {endereco.bairro}, {endereco.localidade} - {endereco.uf} " +
+                                $"(Ibge: {endereco.ibge} | Gia: {endereco.gia} | Ddd: {endereco.ddd} | Siafi: {endereco .siafi})";
+
+
+            using (StreamWriter streamWriter = new StreamWriter(_caminho, true))
+            {
+                streamWriter.WriteLine(log);       
+            }
+        }
+
+        public void ListarLogs()
+        {
+            using(StreamReader streamReader = new StreamReader(_caminho))
+            {
+                while(!streamReader.EndOfStream)
+                {
+                    Console.WriteLine(streamReader.ReadLine());       
+                }
+            }
+        }
+
+    }
+}
